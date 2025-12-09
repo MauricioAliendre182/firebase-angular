6ac8da3a9b89969495cd34cd9b113b03e9a1cff8
Mauricio Aliendre Perez <Maurcio.Aliendre@jala.university>
2025-12-09 18:50:07 -0400

    Add initial structure for components and the corresponding routes

--- angular-firebase-chat/src/app/app.routes.ts
+++ angular-firebase-chat/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/auth',
+    pathMatch: 'full',
+  },
+  {
+    path: 'auth',
+    loadComponent: () => import('./components/auth/auth').then(m => m.Auth),
+    title: 'Log in - Chat assistant',
+  },
+  {
+    path: 'chat',
+    loadComponent: () => import('./components/chat/chat').then(m => m.Chat),
+    title: 'Chat - Assistant',
+  },
+  {
+    path: '**',
+    redirectTo: '/auth',
+  }
+];
