574bd1a4ff3a35987c5142aab9446c31302b2495
Mauricio Aliendre Perez <Maurcio.Aliendre@jala.university>
2025-12-11 14:52:20 -0400

    Add new configuration to app.config to interact with Firebase services

--- angular-firebase-chat/src/app/app.config.ts
+++ angular-firebase-chat/src/app/app.config.ts
@@ -2,10 +2,36 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp } from 'firebase/app';
+import { envrionment } from '../environments/environment.prod';
+import { provideAuth } from '@angular/fire/auth';
+import { getAuth } from 'firebase/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    // We need to pass a function that initializes the Firebase app
+    // this function will contain our env variables
+    // This is to use the DB Firebase services
+    provideFirebaseApp(() => {
+      return initializeApp(envrionment.firebaseConfig)
+    }),
+    // provideAuth is to use the Firebase Auth services
+    // We need to pass a function that initializes the Auth service
+    // getAuth() initializes and returns the Auth instance associated with the default App
+    provideAuth(() => {
+      return getAuth();
+    }),
+    // This is to use the Firestore services for our DB
+    provideFirestore(() => {
+      // We can initialize and return the Firestore instance associated with the default App
+      // using getFirestore() from 'firebase/firestore'
+      return getFirestore();
+    })
   ]
 };
